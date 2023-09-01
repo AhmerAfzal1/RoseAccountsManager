0d208c8ee34bb82471ca747261520a8c38055b29
Ahmer Afzal <ahmerafzal@yahoo.com>
2023-09-01 20:08:57 +0500

    Gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,37 +65,39 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0-rc01")
     implementation("androidx.multidex:multidex:2.0.1")
-    implementation("androidx.compose.material:material-icons-extended:1.6.0-alpha04")
 
     //Jetpack Compose BOM
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
 
-    //Firebase BOM and Ads Services
+    //Firebase BOM
     implementation(platform("com.google.firebase:firebase-bom:32.2.3"))
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-appcheck-playintegrity")
     implementation("com.google.firebase:firebase-crashlytics-ktx")
     implementation("com.google.firebase:firebase-messaging-ktx")
     implementation("com.google.firebase:firebase-perf-ktx")
+
+    //Ads
     implementation("com.google.android.gms:play-services-ads:22.3.0")
 
     //Jetpack Compose View Model
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.1")
 
     //Jetpack Compose Navigation
-    implementation("androidx.navigation:navigation-compose:2.7.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("androidx.navigation:navigation-compose:2.7.1")
 
-    //Dagger Hilt
+    //Dagger - Hilt
     implementation("com.google.dagger:hilt-android:2.47")
     ksp("com.google.dagger:hilt-compiler:2.47")
 
