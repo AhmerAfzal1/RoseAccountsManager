fe206a2d62f8368a2193e39bacb05c38fdd4a6e4
Ahmer Afzal <ahmerafzal@yahoo.com>
2024-03-24 12:01:26 +0500

    Gradle update
    Signed-off-by: Ahmer Afzal <ahmerafzal@yahoo.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.3.0" apply false
+    id("com.android.application") version "8.3.1" apply false
     id("com.google.dagger.hilt.android") version "2.51" apply false
     id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
